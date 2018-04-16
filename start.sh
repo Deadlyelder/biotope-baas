ff2e3d6539da3bf46cf9ea001c614d7e91c0a636
Sankalp <Deadlyelder@users.noreply.github.com>
2018-04-16 13:56:15 +0200

    Clean up start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-BITCOINBITCOIN_NETWORK=regtest CHARGED_HOST=baas.serval.uni.lu CHARGED_ALIAS=deadlykitten CHARGED_IP=10.244.3.13 docker-compose up
-||||||| merged common ancestors
-NBITCOIN_NETWORK=regtest CHARGED_HOST=baas.serval.uni.lu CHARGED_ALIAS=deadlykitten CHARGED_IP=10.244.3.13 docker-compose up
-=======
-NBITCOIN_NETWORK=regtest LETSENCRYPT_EMAIL=deadlyelder@me.com CHARGED_API_TOKEN=abcd1234 CHARGED_HOST=baas.serval.uni.lu CHARGED_ALIAS=deadlykitten CHARGED_IP=10.244.3.13 docker-compose up
->>>>>>> 991841ed15e3352804718dde7c60f370b4326d9d
+NBITCOIN_NETWORK=regtest LETSENCRYPT_EMAIL=deadlyelder@me.com CHARGED_API_TOKEN=7iX6RrfbD0LsZ9fIK CHARGED_HOST=baas.serval.uni.lu CHARGED_ALIAS=deadlykitten CHARGED_IP=10.244.3.13 docker-compose up
